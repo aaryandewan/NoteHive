bd9f8fdb730c3da54be0b8c0d7eb8ebc18a9e8de
Aaryan Dewan <aaryan.dewan@iongroup.com>
2023-07-01 16:12:54 +0530

    complete users man tough

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -56,20 +58,67 @@ router.post('/register', (req, res) => {
   });
 
 
-  
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
+
 router.post('/login', (req, res) => {
-  // Login code here
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 31556926 }, // 1 year in seconds
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
 });
+
+const passport = require('passport');
+
+// ...
 
 // @route GET api/users/current
 // @desc Return current user
 // @access Private
-router.get('/current', (req, res) => {
-  // Return current user code here
-});
+router.get(
+  '/current', 
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
